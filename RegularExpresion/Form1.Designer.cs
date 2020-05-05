4ddca021f79b3f816ea473437e5ae8a7ade0c176
Sergiu Voloc <SergiuVoloc@users.noreply.github.com>
2020-05-05 17:29:11 +0300

    Add project files.

---
+++ RegularExpresion/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace RegularExpression
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CheckButton = new System.Windows.Forms.Button();
+            this.textBoxPattern = new System.Windows.Forms.TextBox();
+            this.textBoxString = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CheckButton
+            // 
+            this.CheckButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.CheckButton.Location = new System.Drawing.Point(278, 348);
+            this.CheckButton.Name = "CheckButton";
+            this.CheckButton.Size = new System.Drawing.Size(223, 54);
+            this.CheckButton.TabIndex = 0;
+            this.CheckButton.Text = "Check";
+            this.CheckButton.UseVisualStyleBackColor = true;
+            this.CheckButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxPattern
+            // 
+            this.textBoxPattern.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBoxPattern.Location = new System.Drawing.Point(46, 232);
+            this.textBoxPattern.Name = "textBoxPattern";
+            this.textBoxPattern.Size = new System.Drawing.Size(272, 35);
+            this.textBoxPattern.TabIndex = 1;
+            this.textBoxPattern.TextChanged += new System.EventHandler(this.textBoxPattern_TextChanged);
+            // 
+            // textBoxString
+            // 
+            this.textBoxString.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBoxString.Location = new System.Drawing.Point(446, 232);
+            this.textBoxString.Name = "textBoxString";
+            this.textBoxString.Size = new System.Drawing.Size(288, 35);
+            this.textBoxString.TabIndex = 1;
+            this.textBoxString.TextChanged += new System.EventHandler(this.textBoxString_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(99, 208);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Enter the Pattern here:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(514, 208);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(155, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Enter the String here:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Noto Sans", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(751, 37);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Check if a string respects a Regular Expression pattern";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxString);
+            this.Controls.Add(this.textBoxPattern);
+            this.Controls.Add(this.CheckButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CheckButton;
+        private System.Windows.Forms.TextBox textBoxPattern;
+        private System.Windows.Forms.TextBox textBoxString;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ RegularExpresion/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+
+
+
+namespace RegularExpression
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Assigning the entered text from text box to a Regex variable
+                Regex myRegularExpression = new Regex(textBoxPattern.Text);
+
+                // Check if the string from right text box respects Regular Expression (pattern) specified above
+                if (myRegularExpression.IsMatch(textBoxString.Text))
+                {
+                    // If it resrects pattern, pop-up an MessageBox with Success text
+                    MessageBox.Show("SUCCESS! String respects the pattern!");
+                }
+                else
+                {
+                    // If it doesn't resrects pattern, pop-up an MessageBox with Error text
+                    MessageBox.Show("ERROR! String doesn't respect the pattern!");
+                }
+            }
+            catch (Exception)
+            {
+                // Catch an error if the pattern inserted respects Regular Expression format
+                // And return an MessageBox with specific error
+                MessageBox.Show("ERROR! The pattern doesn't respect Regular Expression format!");
+            }
+          
+        }
+
+        private void textBoxPattern_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textBoxString_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
